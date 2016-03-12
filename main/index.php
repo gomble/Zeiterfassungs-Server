2860725b9a52a230015f1aa11b3f08d53cc858f6
gomble <goekhanbalta@live.de>
2016-03-13 00:24:46 +0100

    Index & CSS angelegt

--- main/index.php
+++ main/index.php
@@ -1,19 +1,38 @@
-<?php 
-echo "test";
-
-echo "test3";
-echo "19:30";
-
-echo "19:37";
-
-
-echo "19:38";
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<title>Zeiterfassung</title>
+</head>
+<body>
 
 
+	<div id="wrapper">
+
+		<div id="menu">
+			<div id="menu-login">
+			
+				<section class="loginform cf">
+					<form name="login" action="index_submit" method="get"  accept-charset="utf-8">
+						<ul>
+							<li><label for="usermail">Email</label> <input type="email"
+								name="usermail" placeholder="yourname@email.com" required></li>
+							<li><label for="password">Password</label> <input type="password"
+								name="password" placeholder="password" required></li>
+							<li><input type="submit" value="Login"></li>
+						</ul>
+					</form>
+				</section>
+				
+			</div>
+		</div>
+		<div id="header"></div>
+		<div id="footer"></div>
 
 
-echo "19:50";
+	</div>
 
-echo "19:38";
-?>
-aaaaaaaaa
+
+</body>
+</html>

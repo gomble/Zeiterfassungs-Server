55d492efb0ef9803600f6b874a89330042ce2fce
gomble <goekhanbalta@live.de>
2016-09-08 09:02:14 +0200

    Update geodata-class.php

--- main/views/geodata-class.php
+++ main/views/geodata-class.php
@@ -21,28 +21,34 @@ class Geodata {
 	public $messages = array ();
 	private $logged_in_user = null;
 	private $coords = array ();
+	
+	
 	private $month = null;
 	private $dateform = null;
 	
-	/**
-	 * the function "__construct()" automatically starts whenever an object of this class is created
-	 */
+
+	
+	
 	public function __construct() {
+		
 		if (isset ( $_SESSION ["user_email"] )) {
 			
 			$this->logged_in_user = $_SESSION ["user_email"];
 			
-			$this->month = $this->get_current_month ();
-			
-			$this->dateform = $this->get_current_daymonthyear ();
-			
-			if (isset ( $_POST ["map_submit"]) || isset ( $_POST ["gps_submit"] )) {
-				$this->change_daymonthyear ();
+			if($this->db_connection()){
+				
+				$this->month = $this->get_current_month ();
+				$this->dateform = $this->get_current_daymonthyear ();
+				
+				if (isset ( $_POST ["map_submit"]) || isset ( $_POST ["gps_submit"] ) ) {
+					$this->change_daymonthyear ();
+					$this->change_month ();
+				}
+
 			}
-		
 			
-			echo $this->month;
 		}
+		
 	}
 	private function change_month() {
 		if (! empty ( $_POST ['gps_date'] )) {
@@ -75,18 +81,11 @@ class Geodata {
 	/**
 	 * get coordinates from the user who is logged in
 	 */
-	public function get_coordinates() {
-		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+	public function get_coordinates($dateformat) {
 		
-		// change character set to utf8 and check it
-		if (! $this->db_connection->set_charset ( "utf8" )) {
-			$this->errors [] = $this->db_connection->error;
-		}
-		
-		// if no connection errors (= working database connection)
-		if (! $this->db_connection->connect_errno) {
+		if ($this->db_connection()) {
 			
-			$sql = "SELECT geo.lat, geo.long, geo.timestamp FROM geo WHERE geo.timestamp LIKE '" . $this->dateform . "%' AND geo.user= '" . $this->logged_in_user . "';";
+			$sql = "SELECT geo.lat, geo.long, geo.timestamp FROM geo WHERE geo.timestamp LIKE '" . $dateformat . "%' AND geo.user= '" . $this->logged_in_user . "';";
 			$query_check_coords = $this->db_connection->query ( $sql );
 			
 			if ($query_check_coords = $this->db_connection->query ( $sql )) {
@@ -112,15 +111,8 @@ class Geodata {
 		}
 	}
 	public function get_workplace_coords() {
-		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		
-		// change character set to utf8 and check it
-		if (! $this->db_connection->set_charset ( "utf8" )) {
-			$this->errors [] = $this->db_connection->error;
-		}
-		
-		// if no connection errors (= working database connection)
-		if (! $this->db_connection->connect_errno) {
+		if ($this->db_connection()) {
 			
 			$sql = "SELECT workplace.lat, workplace.long FROM workplace;";
 			$query_check_coords = $this->db_connection->query ( $sql );
@@ -147,15 +139,8 @@ class Geodata {
 		}
 	}
 	public function get_workplace_coords_polygon() {
-		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		
-		// change character set to utf8 and check it
-		if (! $this->db_connection->set_charset ( "utf8" )) {
-			$this->errors [] = $this->db_connection->error;
-		}
-		
-		// if no connection errors (= working database connection)
-		if (! $this->db_connection->connect_errno) {
+		if ($this->db_connection()) {
 			
 			$sql = "SELECT workplace.lat, workplace.long FROM workplace;";
 			$query_check_coords = $this->db_connection->query ( $sql );
@@ -183,21 +168,50 @@ class Geodata {
 		}
 	}
 	
+	
+	
+	public function get_workplace_coords_polygon_buchung() {
+		
+		if ($this->db_connection()) {
+			
+			$sql = "SELECT workplace.lat, workplace.long FROM workplace;";
+			$query_check_coords = $this->db_connection->query ( $sql );
+			
+			if ($query_check_coords = $this->db_connection->query ( $sql )) {
+				
+				$number_of_rows = $query_check_coords->num_rows;
+				$current_row = 0;
+				
+				$gps_coordinate_workplace = array();
+				
+				/* fetch object array */
+				while ( $row = $query_check_coords->fetch_row () ) {
+					
+					$temp = $row [0]." ".$row [1];
+					
+					$gps_coordinate_workplace[] = $temp;
+					
+				}
+				
+				/* free result set */
+				$query_check_coords->close ();
+			}
+		}
+		return $gps_coordinate_workplace;
+	}
+	
+	
+	
+	
+	
 	/**
 	 * get coordinates from the user who is logged in
 	 */
-	public function get_gps_data() {
-		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
-		
-		// change character set to utf8 and check it
-		if (! $this->db_connection->set_charset ( "utf8" )) {
-			$this->errors [] = $this->db_connection->error;
-		}
-		
-		// if no connection errors (= working database connection)
-		if (! $this->db_connection->connect_errno) {
+	public function get_gps_data_print($date_format) {
+
+		if ($this->db_connection()) {
 			
-			$sql = "SELECT geo.lat, geo.long, DATE_FORMAT(timestamp,'%d.%m.%Y %T') AS datum FROM geo WHERE geo.timestamp LIKE '" . $this->dateform . "%' AND geo.user = '" . $this->logged_in_user . "';";
+			$sql = "SELECT geo.lat, geo.long, DATE_FORMAT(timestamp,'%d.%m.%Y %T') AS datum FROM geo WHERE geo.timestamp LIKE '" . $date_format . "%' AND geo.user = '" . $this->logged_in_user . "' ORDER BY datum ASC;";
 			$query_check_coords = $this->db_connection->query ( $sql );
 			
 			if ($query_check_coords = $this->db_connection->query ( $sql )) {
@@ -215,6 +229,66 @@ class Geodata {
 			}
 		}
 	}
+	
+		public function get_gps_data($date_format) {
+
+		if ($this->db_connection()) {
+			
+			$sql = "SELECT geo.lat, geo.long, DATE_FORMAT(timestamp,'%d.%m.%Y %T') AS datum FROM geo WHERE geo.timestamp LIKE '" . $date_format . "%' AND geo.user = '" . $this->logged_in_user . "' ORDER BY datum ASC;";
+			$query_check_coords = $this->db_connection->query ( $sql );
+			
+			if ($query_check_coords = $this->db_connection->query ( $sql )) {
+				while ( $row = $query_check_coords->fetch_row () ) {
+					$gps_coordinate_timestamp[] = 	array($row [2], $row [0], $row [1]);
+																		
+																		
+				}
+				$query_check_coords->close ();
+			}
+		}
+		return $gps_coordinate_timestamp;
+	}
+	
+	
+	
+	public function db_connection() {
+		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+		
+		// change character set to utf8 and check it
+		if (! $this->db_connection->set_charset ( "utf8" )) {
+			$this->errors [] = $this->db_connection->error;
+		}
+		
+		if (! $this->db_connection->connect_errno) {
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	
+	public function calc_time($datum,$datum2) {
+			
+			$start = date_create($datum);
+			$end = date_create($datum2);
+			$diff=date_diff($end,$start);
+			
+			return $diff->format('%H:%I:%S');
+
+	}	
+	
+	public function calc_time_date($datum) {
+			$d=strtotime($datum);
+			return date("d-m-Y", $d);
+	}	
+	
+	public function calc_time_hour_minute($datum) {
+			$d=strtotime($datum);
+			return date("H:i:s", $d);
+	}	
+	
+	
 }
 
 ?>
